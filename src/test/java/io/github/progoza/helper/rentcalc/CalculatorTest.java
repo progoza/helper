80f3f07872d397b3f9e915b09249e8a176d48f76
Pawel Rogoza <pawel.rogoza@gmail.com>
2025-03-13 00:18:47 +0100

    added previously unsaved file

--- src/test/java/io/github/progoza/helper/rentcalc/CalculatorTest.java
+++ src/test/java/io/github/progoza/helper/rentcalc/CalculatorTest.java
@@ -1,7 +1,6 @@
 package io.github.progoza.helper.rentcalc;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.List;
